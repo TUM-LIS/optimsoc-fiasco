5cd66ca23180caacaa0d0338a5110b45309b2f0f
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2014-09-05 18:26:54 +0200

    L4/uclibc: Bring in recent port of uClibc

--- l4/pkg/uclibc/lib/contrib/uclibc/libc/sysdeps/linux/or1k/sys/ucontext.h
+++ l4/pkg/uclibc/lib/contrib/uclibc/libc/sysdeps/linux/or1k/sys/ucontext.h
@@ -21,6 +21,19 @@
 
 #include <features.h>
 #include <signal.h>
-#include <asm/ucontext.h>
+
+// From Linux Kernel:
+#ifndef __ASM_GENERIC_UCONTEXT_H
+#define __ASM_GENERIC_UCONTEXT_H
+
+struct ucontext {
+	unsigned long	  uc_flags;
+	struct ucontext  *uc_link;
+	stack_t		  uc_stack;
+	struct sigcontext uc_mcontext;
+	sigset_t	  uc_sigmask;	/* mask last for extensibility */
+};
+
+#endif /* __ASM_GENERIC_UCONTEXT_H */
 
 #endif /* sys/ucontext.h */
