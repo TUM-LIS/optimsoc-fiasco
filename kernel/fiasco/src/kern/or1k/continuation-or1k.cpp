096a380863f463c56583a3169bae6474b0a928c8
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2015-01-19 16:56:04 +0100

    Cleanup application specifics

--- kernel/fiasco/src/kern/or1k/continuation-or1k.cpp
+++ kernel/fiasco/src/kern/or1k/continuation-or1k.cpp
@@ -27,55 +27,36 @@ public:
   bool valid() const
   { return _ip != ~0UL; }
 
-  Address ip() const { return _ip; }
-  void ip(Address ip) { _ip = ip; }
+  Address ip() const { }
+  void ip(Address ip) { }
 
-  Mword flags(Return_frame const *) const { return _psr; }
-  void flags(Return_frame *, Mword psr) { _psr = psr; }
+  Mword flags(Return_frame const *) const {  }
+  void flags(Return_frame *, Mword psr) {  }
 
-  Mword sp(Return_frame const *o) const { return o->usp; }
-  void sp(Return_frame *o, Mword sp) { o->usp = sp; }
+  Mword sp(Return_frame const *o) const {  }
+  void sp(Return_frame *o, Mword sp) { }
 
   void save(Return_frame const *regs)
   {
-    _ip  = regs->ip();
-    //_psr = regs->psr;
   }
 
   void activate(Return_frame *regs, void *cont_func)
   {
-    save(regs);
-    (void)cont_func;
-    //regs->pc = Mword(cont_func);
-    //regs->psr &= ...;
   }
 
   void set(Return_frame *dst, User_return_frame const *src)
   {
-    dst->usp = src->usp;
-    dst->ulr = src->ulr;
-    //dst->km_lr = src->km_lr;
-    _ip = src->pc;
-    _psr = src->psr;
   }
 
   void get(User_return_frame *dst, Return_frame const *src) const
   {
-    dst->usp = src->usp;
-    dst->ulr = src->ulr;
-    //dst->km_lr = src->km_lr;
-    dst->pc = _ip;
-    dst->psr = _psr;
+
   }
 
   void clear() { _ip = ~0UL; }
 
   void restore(Return_frame *regs)
   {
-    //regs->pc = _ip;
-   // regs->psr = _psr;
-    (void)regs;
-    clear();
   }
 
 };
